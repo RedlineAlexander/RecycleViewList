f90fb349aa6095f482a9e485e6615132d4898841
RedlineAlexander <redlinerailmetro@gmail.com>
2019-05-17 11:34:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/recycleviewlist/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.recycleviewlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+private RecyclerView numbersList;
+private NumbersAdapter numbersAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        numbersList = findViewById(R.id.rv_numbers);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        numbersList.setLayoutManager(layoutManager);
+
+        numbersList.setHasFixedSize(true);
+
+        numbersAdapter = new NumbersAdapter(100);
+        numbersList.setAdapter(numbersAdapter);
+
+
+    }
+}
